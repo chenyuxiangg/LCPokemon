91c7680786d520d81d19a5e36c403668c7d81142
chenyuxiangg <1007423165@qq.com>
2024-01-14 12:09:29 +0800

    [owner] chenyuxiang [description] 初步实现战斗场景切换 [version] 0.3

---
+++ Scripts/battle.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func set_roll_grass_material(resource_name, value):
+	get_node("grass_battle/roll_grass").material.set_shader_param(resource_name, value)
+	
+func set_roll_grass_shadow_material(resource_name, value):
+	get_node("grass_battle/roll_grass_shadow").material.set_shader_param(resource_name, value)
